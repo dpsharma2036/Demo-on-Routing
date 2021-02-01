dc091a58f62bcca6322067531f1e7c0a77487804
dpsha@DESKTOP-EJNNPU <dpsharma2036@gmail.com>
2021-01-31 21:54:33 -0500

    initial commit

---
+++ src/department.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { useState, useEffect } from "react";
+
+
+function Department() {
+    const [departments, setDepartments]=useState([]);
+    useEffect(()=>{
+        fetch("https:localhost:44306/api/Dept")
+        .then(res => res.json())
+        .then(
+            (result)=>{
+                setDepartments(result);
+            }
+        );
+    });
+
+    
+return(
+    <div>
+        <h2>departments Data...</h2>
+        <table>
+            <thead>
+                <tr>
+                    <th>Id</th>
+                    <th>Name</th>
+                </tr>
+            </thead>
+
+            <tbody>
+                {departments.map(d =>(
+                    <tr key={d.Id}>
+                        <td>{d.Id}</td>
+                        <td>{d.Name}</td>
+
+                    </tr>
+                ))}
+            </tbody>
+        </table>
+    </div>
+);
+
+}
+
+export default Department;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Link, Switch, Route} from 'react-router-dom';
+import Employee from './employee';
+import Department from './department';
+import Project from './project';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+
+function App() {
+  return(
+    <div>
+      <h2>Welcome to App Component...</h2>
+
+      <ul>
+        <li><Link to="/employees">Employees</Link></li>
+        <li><Link to="/department">Departments</Link></li>
+        <li><Link to="/projects">Projects</Link></li>
+      </ul>
+      <Route path="/employees" component={Employee}></Route>
+      <Route path="/departments" component={Department}></Route>
+      <Route path="/projects" component={Project}></Route>
+    </div>
+  )
+}
+
+
+ReactDOM.render(<BrowserRouter>
+  <App>
+    
+  </App>
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vita
